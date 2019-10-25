e12ac37cd9dd547d4a2481a79bf4e9bbe6ff2624
simon <>
2019-10-25 21:25:37 +0200

    Add board gui

---
+++ source/gui/board.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from typing import List, Dict
+
+from source.core.board import Board as CoreBoard
+from source.core.card import CombatRow
+from source.core.player import Player
+from source.gui.card import Card
+
+
+class Board(tk.Frame):
+
+    def __init__(self, board: CoreBoard, player: Player):
+        super().__init__(width=1920, height=1080 / 2)
+        self.board = board
+        self.player = player
+        self.card_list: List[Card] = []
+
+    def redraw(self):
+        self.clear_cards()
+        frames = self._get_frames_per_player()
+
+        enemy = self.board.get_enemy_player(self.player)
+        frames[enemy].pack(in_=self)
+        frames[self.player].pack(in_=self)
+
+    def _get_frames_per_player(self) -> Dict[Player, tk.Frame]:
+
+        frame_dict: Dict[Player, tk.Frame] = {}
+
+        combat_row_sorting = [CombatRow.CLOSE, CombatRow.RANGE, CombatRow.SIEGE]
+        for player, card_collection in self.board.cards:
+            frame = tk.Frame()
+            frame_dict[player] = frame
+            for card_list in [card_collection[row] for row in combat_row_sorting]:
+                self._draw_row(card_list, frame)
+
+            self._draw_row(player.active_cards.get_all_cards(), frame)
+
+        return frame_dict
+
+    def clear_cards(self):
+        for card in self.card_list:
+            card.destroy()
+
+    def _draw_row(self, card_list: List[Card], master: tk.Widget):
+        frame = tk.Frame()
+        frame.pack(in_=master)
+        for card in card_list:
+            card.pack(in_=frame, side=tk.RIGHT, padx=Card.WIDTH * 0.1, pady=Card.HEIGHT * 0.1)
--- source/gui/card.py
+++ source/gui/card.py
@@ -4,11 +4,13 @@ from source.core.card import Card as CoreCard
 
 
 class Card(tk.Canvas):
+    HEIGHT = 160
+    WIDTH = 90
 
     def __init__(self, card: CoreCard):
-        super().__init__(height=160, width=90)
+        super().__init__(height=Card.HEIGHT, width=Card.WIDTH)
         self.card = card
-        self.create_rectangle(1, 1, self.cget('width'), self.cget('height'), activeoutline="red")
+        self.create_rectangle(1, 1, Card.WIDTH, Card.HEIGHT, activeoutline="red")
         self.create_text(10, 10, text=str(self.card.damage), font="Times 20")
-        self.create_text(int(self.cget('width')) * 0.7, int(self.cget('height')) * 0.9, text=self.card.ability.name,
-                         font="Times 10")
+        self.create_text(Card.WIDTH * 0.7, Card.HEIGHT * 0.9, text=self.card.ability.name, font="Times 10")
+        self.create_text(Card.WIDTH * 0.5, Card.HEIGHT * 0.5, text=self.card.combat_row.name, font="Times 10")
