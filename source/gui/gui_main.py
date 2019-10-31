1516c25baf49ce5531d241a451d922efb1df9104
simon <>
2019-10-31 18:52:53 +0100

    Add test to gui main

--- source/gui/gui_main.py
+++ source/gui/gui_main.py
@@ -9,26 +9,29 @@ from source.gui.card import Card
 from source.core.board import Board as CoreBoard
 from source.gui.game import Game
 
+master = tk.Tk()
+player1 = Player(0, Faction.NILFGAARD, get_cards(Faction.NILFGAARD))
+player2 = Player(1, Faction.NOTHERN_REALMS, get_cards(Faction.NOTHERN_REALMS))
+environment = GameEnvironment(player1, player2)
+environment.current_player = player1
+card = player1.active_cards.get_all_cards()[0]
+environment.step(player1, card.combat_row, card)
 
-def main():
-    master = tk.Tk()
-    player1 = Player(0, Faction.NILFGAARD, get_cards(Faction.NILFGAARD))
-    player2 = Player(1, Faction.NOTHERN_REALMS, get_cards(Faction.NOTHERN_REALMS))
-    environment = GameEnvironment(player1, player2)
-    environment.current_player = player1
+card = player2.active_cards.get_all_cards()[0]
+environment.step(player2, card.combat_row, card)
+
+card = player1.active_cards.get_all_cards()[0]
+environment.step(player1, card.combat_row, card)
+game = Game(environment, player1)
+game.pack(in_=master)
+master.after(1, game.redraw)
+
+
+def add_card():
     card = player1.active_cards.get_all_cards()[0]
     environment.step(player1, card.combat_row, card)
-
-    card = player2.active_cards.get_all_cards()[0]
-    environment.step(player2, card.combat_row, card)
-
-    card = player1.active_cards.get_all_cards()[0]
-    environment.step(player1, card.combat_row, card)
-    game = Game(environment, player1)
-    game.pack(in_=master)
-    master.after(1, game.redraw)
-    master.mainloop()
+    game.redraw()
 
 
-if __name__ == '__main__':
-    main()
+master.after(1000, add_card)
+master.mainloop()
