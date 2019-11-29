46af752439d4b0602455933377926fe7fc09286e
simon <>
2019-11-29 15:49:18 +0100

    Fix mouse hover highlighting

--- source/gui/card.py
+++ source/gui/card.py
@@ -10,8 +10,9 @@ class Card(tk.Canvas):
     def __init__(self, card: CoreCard, enable_highlighting: bool):
         super().__init__(height=Card.HEIGHT, width=Card.WIDTH, background='white')
         self.card = card
-        fill = 'gold' if card.hero else None
-        self.create_rectangle(1, 1, Card.WIDTH, Card.HEIGHT, fill=fill)
+        self.fill = 'gold' if card.hero else 'white'
+        self.config(background=self.fill)
+        self.create_rectangle(1, 1, Card.WIDTH, Card.HEIGHT)
         self.create_text(15, 15, text=str(self.card.damage), font="Times 20")
         self.create_text(Card.WIDTH * 0.7, Card.HEIGHT * 0.9, text=self.card.ability.name, font="Times 8")
         self.create_text(Card.WIDTH * 0.5, Card.HEIGHT * 0.5, text=self.card.combat_row.name, font="Times 10")
@@ -24,4 +25,4 @@ class Card(tk.Canvas):
         self.config(background='red')
 
     def _stop_highlight(self, _):
-        self.config(background='white')
+        self.config(background=self.fill)
