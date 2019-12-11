7e055bdc8dd264564c30cb6e96bffef606e0458b
simon <>
2019-12-11 18:33:08 +0100

    Add conditional passing to random simulator

--- source/ai/random_simulator.py
+++ source/ai/random_simulator.py
@@ -16,6 +16,9 @@ def simulate_random_game(environment: GameEnvironment, current_player: Player, c
             potential_cards.append(None)  # None == pass
 
         random_card = random.choice(potential_cards)
+        if _should_pass(environment, current_player):
+            random_card = None
+
         if random_card:
             row = random.choice(CombatRow.get_possible_rows(random_card))
         else:
@@ -29,3 +32,14 @@ def _get_potential_cards(player: Player, next_card_source: CardSource) -> List[C
     if next_card_source is CardSource.HAND:
         return player.active_cards.get_all_cards()
     return player.graveyard.get_all_cards()
+
+
+def _should_pass(environment: GameEnvironment, current_player: Player) -> bool:
+    enemy = environment.board.get_enemy_player(current_player)
+    enemy_passed = environment.passed[enemy.id]
+    enemy_damage = environment.board.calculate_damage(enemy)
+    own_damage = environment.board.calculate_damage(current_player)
+
+    if enemy_passed and enemy_damage < own_damage:
+        return True
+    return False
