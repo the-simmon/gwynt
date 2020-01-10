7eba1f6e9976332ded8a0a0cedb17cecd3a35eb8
simon <>
2020-01-10 16:33:01 +0100

    Remove should pass from random simulator

--- source/ai/random_simulator.py
+++ source/ai/random_simulator.py
@@ -11,13 +11,9 @@ def simulate_random_game(environment: GameEnvironment, current_player: Player, c
     game_over = environment.game_over()
     while not game_over:
         potential_cards = _get_potential_cards(current_player, card_source)
-
-        if not environment.passed[current_player.id]:
-            potential_cards.append(None)  # None == pass
+        potential_cards.append(None)  # None == pass
 
         random_card = random.choice(potential_cards)
-        if _should_pass(environment, current_player):
-            random_card = None
 
         if random_card:
             row = random.choice(CombatRow.get_possible_rows(random_card))
@@ -32,14 +28,3 @@ def _get_potential_cards(player: Player, next_card_source: CardSource) -> List[C
     if next_card_source is CardSource.HAND:
         return player.active_cards.get_all_cards()
     return player.graveyard.get_all_cards()
-
-
-def _should_pass(environment: GameEnvironment, current_player: Player) -> bool:
-    enemy = environment.board.get_enemy_player(current_player)
-    enemy_passed = environment.passed[enemy.id]
-    enemy_damage = environment.board.calculate_damage(enemy)
-    own_damage = environment.board.calculate_damage(current_player)
-
-    if enemy_passed and enemy_damage < own_damage:
-        return True
-    return False
