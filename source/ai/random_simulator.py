1c394413d11314bbba2e391fd41651113e1b7158
simon <>
2020-02-11 21:44:52 +0100

    Add leader to random simulator

--- source/ai/random_simulator.py
+++ source/ai/random_simulator.py
@@ -1,7 +1,7 @@
 import random
 from typing import List, Tuple
 
-from source.core.card import Card, Ability
+from source.core.card import Card, Ability, LeaderCard
 from source.core.comabt_row import CombatRow
 from source.core.gameenvironment import GameEnvironment
 from source.core.player import Player
@@ -11,6 +11,8 @@ def simulate_random_game(environment: GameEnvironment) -> Player:
     game_over = environment.game_over()
     while not game_over:
         potential_cards = _get_potential_cards(environment)
+        if leader := environment.next_player.leader:
+            potential_cards.append(leader)
         potential_cards.append(None)  # None == pass
 
         random_card = random.choice(potential_cards)
@@ -24,6 +26,8 @@ def simulate_random_game(environment: GameEnvironment) -> Player:
             row, card = _get_decoy(environment)
             if card:
                 game_over = environment.step_decoy(environment.next_player, row, random_card, card)
+        elif type(random_card) is LeaderCard:
+            game_over = environment.step_leader(environment.next_player, random_card)
         else:
             game_over = environment.step(environment.next_player, row, random_card)
 
