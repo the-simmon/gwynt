6149d3383f5fd774b3816e58f980fc5dde302157
simon <>
2020-02-13 23:56:24 +0100

    Update cheat menu ui

--- source/gui/widgets/cheat_menu.py
+++ source/gui/widgets/cheat_menu.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+from source.core.comabt_row import CombatRow
 from source.core.gameenvironment import CardSource, CardDestination
 from source.gui.widgets.card_editor import CardEditor
 from source.gui.widgets.enum_combobox import EnumCombobox
@@ -16,14 +17,20 @@ class CheatMenu(tk.LabelFrame):
         self.source_box.grid(row=0, column=1)
 
         tk.Label(self, text='Destination').grid(row=0, column=2)
-        self.source_box = EnumCombobox[CardDestination](self, CardDestination, default=CardDestination.BOARD)
-        self.source_box.grid(row=0, column=3)
+        self.destination_box = EnumCombobox[CardDestination](self, CardDestination, default=CardDestination.BOARD)
+        self.destination_box.grid(row=0, column=3)
 
         self.card_editor = CardEditor(self)
         self.card_editor.grid(row=1, column=0, columnspan=4)
 
-        frame = tk.Frame(self)
-        frame.grid(row=2, column=0, columnspan=4)
-        tk.Button(frame, text='Play Card').grid(row=0, column=0)
-        tk.Button(frame, text='Play Leader').grid(row=0, column=1)
-        tk.Button(frame, text='Pass').grid(row=0, column=2)
+        row_frame = tk.Frame(self)
+        row_frame.grid(row=2, column=0, columnspan=4)
+        tk.Label(row_frame, text='Target row').grid(row=0, column=0)
+        self.target_row_box = EnumCombobox[CombatRow](row_frame, CombatRow)
+        self.target_row_box.grid(row=0, column=1)
+
+        button_frame = tk.Frame(self)
+        button_frame.grid(row=3, column=0, columnspan=4)
+        tk.Button(button_frame, text='Play Card').grid(row=0, column=0)
+        tk.Button(button_frame, text='Play Leader').grid(row=0, column=1)
+        tk.Button(button_frame, text='Pass').grid(row=0, column=2)
