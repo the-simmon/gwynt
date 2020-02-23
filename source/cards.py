54d63176a4f30e8198b9713ad4bdc35847c463d0
simon <>
2020-02-23 17:32:35 +0100

    Update cards

--- source/cards.py
+++ source/cards.py
@@ -1,17 +1,31 @@
-from source.core.card import Card, Ability, LeaderCard
+from source.core.card import Card, Ability, LeaderCard, LeaderAbility
 from source.core.cards.flatten import flatten
 from source.core.comabt_row import CombatRow
 from source.core.player import Faction
 
 deck = [
-    Card(CombatRow.CLOSE, 8) * 4,
-    Card(CombatRow.CLOSE, 3, Ability.TIGHT_BOND) * 4,
-    Card(CombatRow.RANGE, 4, hero=True),
-    Card(CombatRow.CLOSE, 5) * 8
+    Card(CombatRow.CLOSE, 15, hero=True) * 2,
+    Card(CombatRow.RANGE, 7, Ability.MEDIC, hero=True),
+    Card(CombatRow.CLOSE, 7, hero=True),
+    Card(CombatRow.CLOSE, 7, Ability.SCORCH),
+    Card(CombatRow.CLOSE, 6),
+    Card(CombatRow.AGILE, 6, Ability.MORALE_BOOST),
+    Card(CombatRow.CLOSE, 5) * 2,
+    Card(CombatRow.CLOSE, 0, Ability.SPY, hero=True),
+    Card(CombatRow.CLOSE, 10, hero=True),
+    Card(CombatRow.CLOSE, 10, Ability.MEDIC, hero=True),
+    Card(CombatRow.SIEGE, 10, hero=True),
+    Card(CombatRow.RANGE, 10, hero=True),
+    Card(CombatRow.RANGE, 10) * 2,
+    Card(CombatRow.SIEGE, 10),
+    Card(CombatRow.RANGE, 6) * 2,
+    Card(CombatRow.CLOSE, 6),
+    Card(CombatRow.SIEGE, 6),
+    Card(CombatRow.CLOSE, 5, Ability.TIGHT_BOND)
 ]
 
 deck = flatten(deck)
 
-leader = LeaderCard(CombatRow.RANGE, 0, Ability.SPECIAL_COMMANDERS_HORN)
+leader = LeaderCard(CombatRow.NONE, 0, Ability.NONE, leader_ability=LeaderAbility.ENEMY_GRAVEYARD2HAND)
 
 faction = Faction.NILFGAARD
